6502f7cc0252b414957fb3fd44fb191fe88756e2
alexanderwodarz <git@alexanderwodarz.de>
2023-07-05 21:44:58 +0200

    Removed web support Add bridge for vue

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { registerPlugin } from '@capacitor/core';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type {PluginImplementations} from "@capacitor/core/types/definitions";
 
 import type { inAppPurchasePlugin } from './definitions';
+import purchase from './purchase';
 
-const inAppPurchase = registerPlugin<inAppPurchasePlugin>('inAppPurchase', {
-  web: () => import('./web').then(m => new m.inAppPurchaseWeb()),
-});
+const inAppPurchase: PluginImplementations = registerPlugin<inAppPurchasePlugin>('inAppPurchase', {});
 
 export * from './definitions';
 export { inAppPurchase };
+export {purchase}
