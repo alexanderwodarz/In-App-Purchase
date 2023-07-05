1f976e7fa9fc5a8abfa0685762a27834b3151747
alexanderwodarz <git@alexanderwodarz.de>
2023-07-05 21:45:12 +0200

    Add new interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -2,8 +2,14 @@ export interface inAppPurchasePlugin {
   list(options: {list: [registerProduct]}): Promise<{value: boolean}>;
   getStoreProducts(): Promise<any>;
   getPurchasedItems(): Promise<any>;
-  getProduct(options: { id: string }): Promise<{ value: string }>;
+  getProduct(options: { id: string }): Promise<{ product: string}>;
   purchase(options: {id: string}): Promise<{value: boolean}>;
+}
+
+export interface product {
+  description: string,
+  name: string,
+  price: string
 }
 
 export interface registerProduct {
